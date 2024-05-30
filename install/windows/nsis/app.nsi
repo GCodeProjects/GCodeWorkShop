u-235/edytornc

Create nsis script

install/windows/nsis/app.nsi
@@ -1,0 +1,325 @@
+; NSIS script file
+
+
+;==============================;
+; Configuration                ;
+;==============================;
+
+!ifndef APP_ARCH
+    !define APP_ARCH x86_64
+!endif
+
+!ifndef INSTALLER_FILE
+    !define INSTALLER_FILE InstallEdytorNC_${APP_ARCH}.exe
+!endif
+
+; Base directories
+
+!ifndef PROJECT_ROOT
+    !define PROJECT_ROOT ${__FILEDIR__}\..\..\..
+!endif
+
+!ifndef BUILD_ROOT
+    !if /FileExists "${BUILD_ROOT}\build\"
+        !define BUILD_ROOT ${PROJECT_ROOT}\build
+    !else
+        !define BUILD_ROOT ${PROJECT_ROOT}
+    !endif
+!endif
+
+; Sources directories
+
+!ifndef SRC_BIN
+    !define SRC_BIN ${PROJECT_ROOT}\bin\release
+!endif
+
+!ifndef SRC_DOC
+    !define SRC_DOC ${PROJECT_ROOT}\doc
+!endif
+
+!ifndef SRC_EXAMPLES
+    !define SRC_EXAMPLES ${PROJECT_ROOT}\examples
+!endif
+
+!ifndef SRC_I18N
+    !define SRC_I18N ${PROJECT_ROOT}\lang
+!endif
+
+!ifndef SRC_CXX
+    !ifdef CXX_RUNTIME
+        !define SRC_CXX ${CXX_RUNTIME}
+    !else
+        !define SRC_CXX ${SRC_BIN}
+    !endif
+!endif
+
+!ifndef SRC_QT
+    !ifdef QT_HOST_BINS
+        !define SRC_QT ${QT_HOST_BINS}
+    !else
+        !define SRC_QT ${SRC_BIN}
+    !endif
+!endif
+
+!ifndef SRC_QT_DEP
+    !ifdef QT_HOST_BINS
+        !define SRC_QT_DEP ${QT_HOST_BINS}
+    !else
+        !define SRC_QT_DEP ${SRC_QT}
+    !endif
+!endif
+
+!ifndef SRC_QT_PLUGINS
+    !ifdef QT_HOST_DATA
+        !define SRC_QT_PLUGINS ${QT_HOST_DATA}\plugins
+    !else
+        !define SRC_QT_PLUGINS ${SRC_QT}
+    !endif
+!endif
+
+!ifndef SRC_QT_I18N
+    !ifdef QT_HOST_DATA
+        !define SRC_QT_I18N ${QT_HOST_DATA}\translations
+    !else
+        !define SRC_QT_I18N ${SRC_QT}\translations
+    !endif
+!endif
+
+; Destination subdirectories
+
+!ifndef DST_BIN
+    !define DST_BIN bin
+!endif
+
+!ifndef DST_DOC
+    !define DST_DOC doc
+!endif
+
+!ifndef DST_EXAMPLES
+    !define DST_EXAMPLES examples
+!endif
+
+!ifndef DST_I18N
+    !define DST_I18N lang
+!endif
+
+
+;==============================;
+; General                      ;
+;==============================;
+
+; The icon for the installer and uninstaller.
+;   MUI_ICON icon_file
+;   MUI_UNICON icon_file
+
+    Unicode true
+    Name "EdytorNC"
+    OutFile "${INSTALLER_FILE}"
+
+!if ${APP_ARCH} == "x86_64"
+    InstallDir "$PROGRAMFILES64\EdytorNC"
+!else
+    InstallDir "$PROGRAMFILES32\EdytorNC"
+!endif
+    
+    InstallDirRegKey HKEY_LOCAL_MACHINE "SOFTWARE\EdytorNC" ""
+;   SetCompressor lzma
+
+
+; Modern UI
+
+    !include "MUI.nsh"
+    !define MUI_ABORTWARNING
+
+
+;==============================;
+; Pages                        ;
+;==============================;
+ 
+    Var STARTMENU_FOLDER
+
+    !insertmacro MUI_PAGE_WELCOME
+    !insertmacro MUI_PAGE_LICENSE "${PROJECT_ROOT}\LICENSE"
+    !insertmacro MUI_PAGE_DIRECTORY
+
+    ;Start Menu Folder Page Configuration
+    !define MUI_STARTMENUPAGE_REGISTRY_ROOT HKEY_LOCAL_MACHINE
+    !define MUI_STARTMENUPAGE_REGISTRY_KEY "SOFTWARE\EdytorNC"
+    !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "Start Menu Folder"
+    !insertmacro MUI_PAGE_STARTMENU Application $STARTMENU_FOLDER
+
+    !insertmacro MUI_PAGE_INSTFILES
+    !insertmacro MUI_PAGE_FINISH
+
+    ; Uninstaller pages
+    !insertmacro MUI_UNPAGE_WELCOME
+    !insertmacro MUI_UNPAGE_CONFIRM
+    !insertmacro MUI_UNPAGE_INSTFILES
+    !insertmacro MUI_UNPAGE_FINISH
+
+
+;==============================;
+; Languages                    ;
+;==============================;
+
+    !insertmacro MUI_LANGUAGE "English"
+    !insertmacro MUI_LANGUAGE "Russian"
+
+
+;==============================;
+; Main section                 ;
+;==============================;
+
+Section "-Install"
+    SetOutPath "$INSTDIR"
+    File /oname=LICENSE.txt "${PROJECT_ROOT}\LICENSE"
+
+    SetOutPath "$INSTDIR\${DST_BIN}"
+    File /oname=EdytorNC.exe "${SRC_BIN}\edytornc.exe"
+    File /oname=SFS.exe "${SRC_BIN}\sfs.exe"
+    File /oname=qt.conf "${__FILEDIR__}\..\qt.conf"
+
+    ;Store installation folder
+    WriteRegStr HKEY_LOCAL_MACHINE "SOFTWARE\EdytorNC" "" "$INSTDIR"
+
+    ;Create uninstaller
+    WriteUninstaller "$INSTDIR\uninst.exe"
+
+    !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+    ;Create shortcuts
+    CreateDirectory "$SMPROGRAMS\$STARTMENU_FOLDER"
+    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\EdytorNC.lnk" "$INSTDIR\${DST_BIN}\EdytorNC.exe"
+    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\Uninstall.lnk" "$INSTDIR\uninst.exe"
+    !insertmacro MUI_STARTMENU_WRITE_END
+SectionEnd
+
+Section "Uninstall"
+    Push $R0
+    !insertmacro MUI_STARTMENU_GETFOLDER Application $R0
+    Delete "$SMPROGRAMS\$R0\Uninstall.lnk"
+    Delete "$SMPROGRAMS\$R0\EdytorNC.lnk"
+    RMDir  "$SMPROGRAMS\$R0"
+    Pop $R0
+
+    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\EdytorNC"
+    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\EdytorNC"
+
+    Delete "$INSTDIR\${DST_BIN}\EdytorNC.exe"
+    Delete "$INSTDIR\${DST_BIN}\SFS.exe"
+    Delete "$INSTDIR\${DST_BIN}\qt.conf"
+    Delete "$INSTDIR\LICENSE.txt"
+    Delete "$INSTDIR\uninst.exe"
+
+    RMDir "$INSTDIR\${DST_BIN}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Internationalisation         ;
+;==============================;
+
+Section "-Install i18n"
+    SetOutPath "$INSTDIR\${DST_I18N}"
+
+    File /oname=edytornc_ca.qm "${SRC_I18N}\edytornc_ca.qm" 
+    File /oname=edytornc_cs_CZ.qm "${SRC_I18N}\edytornc_cs_CZ.qm"
+    File /oname=edytornc_de.qm "${SRC_I18N}\edytornc_de.qm"
+    File /oname=edytornc_es.qm "${SRC_I18N}\edytornc_es.qm"
+    File /oname=edytornc_fi.qm "${SRC_I18N}\edytornc_fi.qm"
+    File /oname=edytornc_nl.qm "${SRC_I18N}\edytornc_nl.qm"
+    File /oname=edytornc_pl.qm "${SRC_I18N}\edytornc_pl.qm"
+    File /oname=edytornc_ru.qm "${SRC_I18N}\edytornc_ru.qm"
+    File /oname=kdiff3_ca.qm "${SRC_I18N}\kdiff3_ca.qm"
+    File /oname=kdiff3_cs_CZ.qm "${SRC_I18N}\kdiff3_cs_CZ.qm"
+    File /oname=kdiff3_de.qm "${SRC_I18N}\kdiff3_de.qm"
+    File /oname=kdiff3_es.qm "${SRC_I18N}\kdiff3_es.qm"
+    File /oname=kdiff3_fi.qm "${SRC_I18N}\kdiff3_fi.qm"
+    File /oname=kdiff3_nl.qm "${SRC_I18N}\kdiff3_nl.qm"
+    File /oname=kdiff3_pl.qm "${SRC_I18N}\kdiff3_pl.qm"
+    File /oname=kdiff3_ru.qm "${SRC_I18N}\kdiff3_ru.qm"
+SectionEnd
+
+Section "un.Install i18n"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_ca.qm" 
+    Delete "$INSTDIR\${DST_I18N}\edytornc_cs_CZ.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_de.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_es.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_fi.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_nl.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_pl.qm"
+    Delete "$INSTDIR\${DST_I18N}\edytornc_ru.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_ca.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_cs_CZ.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_de.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_es.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_fi.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_nl.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_pl.qm"
+    Delete "$INSTDIR\${DST_I18N}\kdiff3_ru.qm"
+
+    RMDir "$INSTDIR\${DST_I18N}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Documentation                ;
+;==============================;
+
+Section "-Install doc"
+    SetOutPath "$INSTDIR\${DST_DOC}"
+
+    File /oname=EdytorNC_SerialTransmission_Help.html "${SRC_DOC}\EdytorNC_SerialTransmission_Help.html"
+SectionEnd
+
+Section "un.Install doc"
+    Delete "$INSTDIR\${DST_DOC}\EdytorNC_SerialTransmission_Help.html"
+
+    RMDir "$INSTDIR\${DST_DOC}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Examples                     ;
+;==============================;
+
+Section "-Install Examples"
+    SetOutPath "$INSTDIR\${DST_EXAMPLES}"
+
+    File /oname=0100.nc "${SRC_EXAMPLES}\0100.nc"
+    File /oname=FANUC0M.nc "${SRC_EXAMPLES}\FANUC0M.nc"
+    File /oname=FANUC0T.nc "${SRC_EXAMPLES}\FANUC0T.nc"
+    File /oname=FLANGE.nc "${SRC_EXAMPLES}\FLANGE.nc"
+    File /oname=HAAS.nc "${SRC_EXAMPLES}\HAAS.nc"
+    File /oname=HK.nc "${SRC_EXAMPLES}\HK.nc"
+    File /oname=OSP5020M.nc "${SRC_EXAMPLES}\OSP5020M.nc"
+    File /oname=OSP7000L.nc "${SRC_EXAMPLES}\OSP7000L.nc"
+    File /oname=PHILIPS.nc "${SRC_EXAMPLES}\PHILIPS.nc"
+    File /oname=SINUMERIK840D.nc "${SRC_EXAMPLES}\SINUMERIK840D.nc"
+    File /oname=SINUMERIK850.nc "${SRC_EXAMPLES}\SINUMERIK850.nc"
+    File /oname=TOKARKA.nc "${SRC_EXAMPLES}\TOKARKA.NC"
+    File /oname=cnc_tips.txt "${SRC_EXAMPLES}\cnc_tips.txt"
+    File /oname=macro7.nc "${SRC_EXAMPLES}\macro7.nc"
+SectionEnd
+
+Section "un.Install Examples"
+    Delete "$INSTDIR\${DST_EXAMPLES}\0100.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FANUC0M.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FANUC0T.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FLANGE.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\HAAS.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\HK.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\OSP5020M.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\OSP7000L.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\PHILIPS.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\SINUMERIK840D.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\SINUMERIK850.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\TOKARKA.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\cnc_tips.txt"
+    Delete "$INSTDIR\${DST_EXAMPLES}\macro7.nc"
+
+    RMDir "$INSTDIR\${DST_EXAMPLES}"
+    RMDir "$INSTDIR"
+SectionEnd

install/windows/nsis/mingw.nsi
@@ -1,0 +1,86 @@
+; NSIS script file
+
+
+!include qt.nsi
+
+
+;==============================;
+; MINGW section                ;
+;==============================;
+
+Section "-Install dependies"
+    SetOutPath "$INSTDIR\${DST_BIN}"
+
+    File /oname=libgcc_s_dw2-1.dll "${SRC_CXX}\libgcc_s_dw2-1.dll"
+    File /oname=libstdc++-6.dll "${SRC_CXX}\libstdc++-6.dll"
+    File /oname=libwinpthread-1.dll "${SRC_CXX}\libwinpthread-1.dll"
+
+!if /FileExists "${SRC_QT_DEP}\libEGL.dll"
+    ; MINGW from Qt
+    File /oname=libEGL.dll "${SRC_QT_DEP}\libEGL.dll"
+    File /oname=libGLESv2.dll "${SRC_QT_DEP}\libGLESv2.dll"
+!else
+    ; MSYS2
+    File /oname=libbrotlicommon.dll "${SRC_QT_DEP}\libbrotlicommon.dll"
+    File /oname=libbrotlidec.dll "${SRC_QT_DEP}\libbrotlidec.dll"
+    File /oname=libbz2-1.dll "${SRC_QT_DEP}\libbz2-1.dll"
+    File /oname=libdouble-conversion.dll "${SRC_QT_DEP}\libdouble-conversion.dll"
+    File /oname=libfreetype-6.dll "${SRC_QT_DEP}\libfreetype-6.dll"
+    File /oname=libglib-2.0-0.dll "${SRC_QT_DEP}\libglib-2.0-0.dll"
+    File /oname=libgraphite2.dll "${SRC_QT_DEP}\libgraphite2.dll"
+    File /oname=libharfbuzz-0.dll "${SRC_QT_DEP}\libharfbuzz-0.dll"
+    File /oname=libiconv-2.dll "${SRC_QT_DEP}\libiconv-2.dll"
+    File /oname=libicudt72.dll "${SRC_QT_DEP}\libicudt72.dll"
+    File /oname=libicuin72.dll "${SRC_QT_DEP}\libicuin72.dll"
+    File /oname=libicuio72.dll "${SRC_QT_DEP}\libicuio72.dll"
+    File /oname=libicutest72.dll "${SRC_QT_DEP}\libicutest72.dll"
+    File /oname=libicutu72.dll "${SRC_QT_DEP}\libicutu72.dll"
+    File /oname=libicuuc72.dll "${SRC_QT_DEP}\libicuuc72.dll"
+    File /oname=libintl-8.dll "${SRC_QT_DEP}\libintl-8.dll"
+    File /oname=libmd4c.dll "${SRC_QT_DEP}\libmd4c.dll"
+    File /oname=libpcre2-16-0.dll "${SRC_QT_DEP}\libpcre2-16-0.dll"
+    File /oname=libpcre2-8-0.dll "${SRC_QT_DEP}\libpcre2-8-0.dll"
+    File /oname=libpng16-16.dll "${SRC_QT_DEP}\libpng16-16.dll"
+    File /oname=libzstd.dll "${SRC_QT_DEP}\libzstd.dll"
+    File /oname=zlib1.dll "${SRC_QT_DEP}\zlib1.dll"
+!endif
+SectionEnd
+
+Section "un.Install dependies"
+    Delete "$INSTDIR\${DST_BIN}\libgcc_s_dw2-1.dll"
+    Delete "$INSTDIR\${DST_BIN}\libstdc++-6.dll"
+    Delete "$INSTDIR\${DST_BIN}\libwinpthread-1.dll"
+
+!if /FileExists "${SRC_QT_DEP}\libEGL.dll"
+    ; MINGW from Qt
+    Delete "$INSTDIR\${DST_BIN}\libEGL.dll"
+    Delete "$INSTDIR\${DST_BIN}\libGLESv2.dll"
+!else
+    ; MSYS2
+    Delete "$INSTDIR\${DST_BIN}\libbrotlicommon.dll"
+    Delete "$INSTDIR\${DST_BIN}\libbrotlidec.dll"
+    Delete "$INSTDIR\${DST_BIN}\libbz2-1.dll"
+    Delete "$INSTDIR\${DST_BIN}\libdouble-conversion.dll"
+    Delete "$INSTDIR\${DST_BIN}\libfreetype-6.dll"
+    Delete "$INSTDIR\${DST_BIN}\libglib-2.0-0.dll"
+    Delete "$INSTDIR\${DST_BIN}\libgraphite2.dll"
+    Delete "$INSTDIR\${DST_BIN}\libharfbuzz-0.dll"
+    Delete "$INSTDIR\${DST_BIN}\libiconv-2.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicudt72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicuin72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicuio72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicutest72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicutu72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libicuuc72.dll"
+    Delete "$INSTDIR\${DST_BIN}\libintl-8.dll"
+    Delete "$INSTDIR\${DST_BIN}\libmd4c.dll"
+    Delete "$INSTDIR\${DST_BIN}\libpcre2-16-0.dll"
+    Delete "$INSTDIR\${DST_BIN}\libpcre2-8-0.dll"
+    Delete "$INSTDIR\${DST_BIN}\libpng16-16.dll"
+    Delete "$INSTDIR\${DST_BIN}\libzstd.dll"
+    Delete "$INSTDIR\${DST_BIN}\zlib1.dll"
+!endif
+
+    RMDir "$INSTDIR\${DST_BIN}"
+    RMDir "$INSTDIR"
+SectionEnd

GCodeProjects/GCodeWorkShop

install/windows/nsis/app.nsi
@@ -1,0 +1,328 @@
+; NSIS script file
+
+
+;==============================;
+; Configuration                ;
+;==============================;
+
+!ifndef APP_ARCH
+    !define APP_ARCH x86_64
+!endif
+
+!ifndef INSTALLER_FILE
+    !define INSTALLER_FILE GCodeWorkShop_${APP_ARCH}_Setup.exe
+!endif
+
+; Base directories
+
+!ifndef PROJECT_ROOT
+    !define PROJECT_ROOT ${__FILEDIR__}\..\..\..
+!endif
+
+!ifndef BUILD_ROOT
+    !if /FileExists "${BUILD_ROOT}\build\"
+        !define BUILD_ROOT ${PROJECT_ROOT}\build
+    !else
+        !define BUILD_ROOT ${PROJECT_ROOT}
+    !endif
+!endif
+
+; Sources directories
+
+!ifndef APP_BINS
+    !define APP_BINS ${PROJECT_ROOT}\bin\release
+!endif
+
+!ifndef APP_DOCS
+    !define APP_DOCS ${PROJECT_ROOT}\doc
+!endif
+
+!ifndef APP_EXAMPLES
+    !define APP_EXAMPLES ${PROJECT_ROOT}\examples
+!endif
+
+!ifndef APP_I18N
+    !define APP_I18N ${PROJECT_ROOT}\lang
+!endif
+
+!ifndef CXX_BINS
+    !ifdef CXX_RUNTIME
+        !define CXX_BINS ${CXX_RUNTIME}
+    !else
+        !define CXX_BINS ${APP_BINS}
+    !endif
+!endif
+
+!ifndef QT_BINS
+    !ifdef QT_HOST_BINS
+        !define QT_BINS ${QT_HOST_BINS}
+    !else
+        !define QT_BINS ${APP_BINS}
+    !endif
+!endif
+
+!ifndef QT_DEP_BINS
+    !define QT_DEP_BINS ${QT_BINS}
+!endif
+
+!ifndef QT_PLUGINS
+    !ifdef QT_HOST_DATA
+        !define QT_PLUGINS ${QT_HOST_DATA}\plugins
+    !else
+        !define QT_PLUGINS ${QT_BINS}
+    !endif
+!endif
+
+!ifndef QT_I18NS
+    !ifdef QT_HOST_DATA
+        !define QT_I18NS ${QT_HOST_DATA}\translations
+    !else
+        !define QT_I18NS ${QT_BINS}\translations
+    !endif
+!endif
+
+; Destination subdirectories
+
+!ifndef DST_BINS
+    !define DST_BINS bin
+!endif
+
+!ifndef DST_DOCS
+    !define DST_DOCS doc
+!endif
+
+!ifndef DST_EXAMPLES
+    !define DST_EXAMPLES examples
+!endif
+
+!ifndef DST_I18NS
+    !define DST_I18NS lang
+!endif
+
+
+;==============================;
+; General                      ;
+;==============================;
+
+; The icon for the installer and uninstaller.
+;   MUI_ICON icon_file
+;   MUI_UNICON icon_file
+
+    Unicode true
+    Name "GCodeWorkShop"
+    OutFile "${INSTALLER_FILE}"
+
+!if ${APP_ARCH} == "x86_64"
+    InstallDir "$PROGRAMFILES64\GCodeWorkShop"
+!else
+    InstallDir "$PROGRAMFILES32\GCodeWorkShop"
+!endif
+    
+    InstallDirRegKey HKEY_LOCAL_MACHINE "SOFTWARE\GCodeWorkShop" ""
+;   SetCompressor lzma
+
+
+; Modern UI
+
+    !include "MUI.nsh"
+    !define MUI_ABORTWARNING
+
+
+;==============================;
+; Pages                        ;
+;==============================;
+ 
+    Var STARTMENU_FOLDER
+
+    !insertmacro MUI_PAGE_WELCOME
+    !insertmacro MUI_PAGE_LICENSE "${PROJECT_ROOT}\LICENSE"
+    !insertmacro MUI_PAGE_DIRECTORY
+
+    ;Start Menu Folder Page Configuration
+    !define MUI_STARTMENUPAGE_REGISTRY_ROOT HKEY_LOCAL_MACHINE
+    !define MUI_STARTMENUPAGE_REGISTRY_KEY "SOFTWARE\GCodeWorkShop"
+    !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "Start Menu Folder"
+    !insertmacro MUI_PAGE_STARTMENU Application $STARTMENU_FOLDER
+
+    !insertmacro MUI_PAGE_INSTFILES
+    !insertmacro MUI_PAGE_FINISH
+
+    ; Uninstaller pages
+    !insertmacro MUI_UNPAGE_WELCOME
+    !insertmacro MUI_UNPAGE_CONFIRM
+    !insertmacro MUI_UNPAGE_INSTFILES
+    !insertmacro MUI_UNPAGE_FINISH
+
+
+;==============================;
+; Languages                    ;
+;==============================;
+
+    !insertmacro MUI_LANGUAGE "English"
+    !insertmacro MUI_LANGUAGE "Catalan"
+    !insertmacro MUI_LANGUAGE "Czech"
+    !insertmacro MUI_LANGUAGE "Dutch"
+    !insertmacro MUI_LANGUAGE "Finnish"
+    !insertmacro MUI_LANGUAGE "German"
+    !insertmacro MUI_LANGUAGE "Polish"
+    !insertmacro MUI_LANGUAGE "Russian"
+    !insertmacro MUI_LANGUAGE "Spanish"
+
+
+;==============================;
+; Main section                 ;
+;==============================;
+
+Section "-Install"
+    SetOutPath "$INSTDIR"
+    File /oname=LICENSE.txt "${PROJECT_ROOT}\LICENSE"
+
+    SetOutPath "$INSTDIR\${DST_BINS}"
+    File /oname=GCodeWorkShop.exe "${APP_BINS}\gcodeworkshop.exe"
+    File /oname=GCodeFileServer.exe "${APP_BINS}\gcodefileserver.exe"
+    File /oname=qt.conf "${__FILEDIR__}\..\qt.conf"
+
+    ;Store installation folder
+    WriteRegStr HKEY_LOCAL_MACHINE "SOFTWARE\GCodeWorkShop" "" "$INSTDIR"
+
+    ;Create uninstaller
+    WriteUninstaller "$INSTDIR\uninst.exe"
+
+    !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+    ;Create shortcuts
+    CreateDirectory "$SMPROGRAMS\$STARTMENU_FOLDER"
+    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\GCodeWorkShop.lnk" "$INSTDIR\${DST_BINS}\GCodeWorkShop.exe"
+    CreateShortCut "$SMPROGRAMS\$STARTMENU_FOLDER\Uninstall.lnk" "$INSTDIR\uninst.exe"
+    !insertmacro MUI_STARTMENU_WRITE_END
+SectionEnd
+
+Section "Uninstall"
+    Push $R0
+    !insertmacro MUI_STARTMENU_GETFOLDER Application $R0
+    Delete "$SMPROGRAMS\$R0\Uninstall.lnk"
+    Delete "$SMPROGRAMS\$R0\GCodeWorkShop.lnk"
+    RMDir  "$SMPROGRAMS\$R0"
+    Pop $R0
+
+    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\GCodeWorkShop"
+    DeleteRegKey HKEY_LOCAL_MACHINE "SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GCodeWorkShop"
+
+    Delete "$INSTDIR\${DST_BINS}\GCodeWorkShop.exe"
+    Delete "$INSTDIR\${DST_BINS}\GCodeFileServer.exe"
+    Delete "$INSTDIR\${DST_BINS}\qt.conf"
+    Delete "$INSTDIR\LICENSE.txt"
+    Delete "$INSTDIR\uninst.exe"
+
+    RMDir "$INSTDIR\${DST_BINS}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Internationalisation         ;
+;==============================;
+
+Section "-Install i18n"
+    SetOutPath "$INSTDIR\${DST_I18NS}"
+
+    File /oname=gcodeworkshop_ca.qm "${APP_I18N}\gcodeworkshop_ca.qm" 
+    File /oname=gcodeworkshop_cs_CZ.qm "${APP_I18N}\gcodeworkshop_cs_CZ.qm"
+    File /oname=gcodeworkshop_de.qm "${APP_I18N}\gcodeworkshop_de.qm"
+    File /oname=gcodeworkshop_es.qm "${APP_I18N}\gcodeworkshop_es.qm"
+    File /oname=gcodeworkshop_fi.qm "${APP_I18N}\gcodeworkshop_fi.qm"
+    File /oname=gcodeworkshop_nl.qm "${APP_I18N}\gcodeworkshop_nl.qm"
+    File /oname=gcodeworkshop_pl.qm "${APP_I18N}\gcodeworkshop_pl.qm"
+    File /oname=gcodeworkshop_ru.qm "${APP_I18N}\gcodeworkshop_ru.qm"
+    File /oname=kdiff3_ca.qm "${APP_I18N}\kdiff3_ca.qm"
+    File /oname=kdiff3_cs_CZ.qm "${APP_I18N}\kdiff3_cs_CZ.qm"
+    File /oname=kdiff3_de.qm "${APP_I18N}\kdiff3_de.qm"
+    File /oname=kdiff3_es.qm "${APP_I18N}\kdiff3_es.qm"
+    File /oname=kdiff3_fi.qm "${APP_I18N}\kdiff3_fi.qm"
+    File /oname=kdiff3_nl.qm "${APP_I18N}\kdiff3_nl.qm"
+    File /oname=kdiff3_pl.qm "${APP_I18N}\kdiff3_pl.qm"
+    File /oname=kdiff3_ru.qm "${APP_I18N}\kdiff3_ru.qm"
+SectionEnd
+
+Section "un.Install i18n"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_ca.qm" 
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_cs_CZ.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_de.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_es.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_fi.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_nl.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_pl.qm"
+    Delete "$INSTDIR\${DST_I18NS}\gcodeworkshop_ru.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_ca.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_cs_CZ.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_de.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_es.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_fi.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_nl.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_pl.qm"
+    Delete "$INSTDIR\${DST_I18NS}\kdiff3_ru.qm"
+
+    RMDir "$INSTDIR\${DST_I18NS}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Documentation                ;
+;==============================;
+
+Section "-Install doc"
+    SetOutPath "$INSTDIR\${DST_DOCS}"
+
+    File /oname=SerialTransmission_Help.html "${APP_DOCS}\SerialTransmission_Help.html"
+SectionEnd
+
+Section "un.Install doc"
+    Delete "$INSTDIR\${DST_DOCS}\SerialTransmission_Help.html"
+
+    RMDir "$INSTDIR\${DST_DOCS}"
+    RMDir "$INSTDIR"
+SectionEnd
+
+
+;==============================;
+; Examples                     ;
+;==============================;
+
+Section "-Install Examples"
+    SetOutPath "$INSTDIR\${DST_EXAMPLES}"
+
+    File /oname=0100.nc "${APP_EXAMPLES}\0100.nc"
+    File /oname=FANUC0M.nc "${APP_EXAMPLES}\FANUC0M.nc"
+    File /oname=FANUC0T.nc "${APP_EXAMPLES}\FANUC0T.nc"
+    File /oname=FLANGE.nc "${APP_EXAMPLES}\FLANGE.nc"
+    File /oname=HAAS.nc "${APP_EXAMPLES}\HAAS.nc"
+    File /oname=HK.nc "${APP_EXAMPLES}\HK.nc"
+    File /oname=OSP5020M.nc "${APP_EXAMPLES}\OSP5020M.nc"
+    File /oname=OSP7000L.nc "${APP_EXAMPLES}\OSP7000L.nc"
+    File /oname=PHILIPS.nc "${APP_EXAMPLES}\PHILIPS.nc"
+    File /oname=SINUMERIK840D.nc "${APP_EXAMPLES}\SINUMERIK840D.nc"
+    File /oname=SINUMERIK850.nc "${APP_EXAMPLES}\SINUMERIK850.nc"
+    File /oname=TOKARKA.nc "${APP_EXAMPLES}\TOKARKA.NC"
+    File /oname=cnc_tips.txt "${APP_EXAMPLES}\cnc_tips.txt"
+    File /oname=macro7.nc "${APP_EXAMPLES}\macro7.nc"
+SectionEnd
+
+Section "un.Install Examples"
+    Delete "$INSTDIR\${DST_EXAMPLES}\0100.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FANUC0M.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FANUC0T.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\FLANGE.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\HAAS.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\HK.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\OSP5020M.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\OSP7000L.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\PHILIPS.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\SINUMERIK840D.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\SINUMERIK850.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\TOKARKA.nc"
+    Delete "$INSTDIR\${DST_EXAMPLES}\cnc_tips.txt"
+    Delete "$INSTDIR\${DST_EXAMPLES}\macro7.nc"
+
+    RMDir "$INSTDIR\${DST_EXAMPLES}"
+    RMDir "$INSTDIR"
+SectionEnd

install/windows/nsis/mingw.nsi
@@ -1,0 +1,85 @@
+; NSIS script file
+
+!include qt.nsi
+
+
+;==============================;
+; MINGW section                ;
+;==============================;
+
+Section "-Install dependies"
+    SetOutPath "$INSTDIR\${DST_BINS}"
+
+    File /oname=libgcc_s_dw2-1.dll "${CXX_BINS}\libgcc_s_dw2-1.dll"
+    File /oname=libstdc++-6.dll "${CXX_BINS}\libstdc++-6.dll"
+    File /oname=libwinpthread-1.dll "${CXX_BINS}\libwinpthread-1.dll"
+
+!if /FileExists "${QT_DEP_BINS}\libEGL.dll"
+    ; MINGW from Qt
+    File /oname=libEGL.dll "${QT_DEP_BINS}\libEGL.dll"
+    File /oname=libGLESv2.dll "${QT_DEP_BINS}\libGLESv2.dll"
+!else
+    ; MSYS2
+    File /oname=libbrotlicommon.dll "${QT_DEP_BINS}\libbrotlicommon.dll"
+    File /oname=libbrotlidec.dll "${QT_DEP_BINS}\libbrotlidec.dll"
+    File /oname=libbz2-1.dll "${QT_DEP_BINS}\libbz2-1.dll"
+    File /oname=libdouble-conversion.dll "${QT_DEP_BINS}\libdouble-conversion.dll"
+    File /oname=libfreetype-6.dll "${QT_DEP_BINS}\libfreetype-6.dll"
+    File /oname=libglib-2.0-0.dll "${QT_DEP_BINS}\libglib-2.0-0.dll"
+    File /oname=libgraphite2.dll "${QT_DEP_BINS}\libgraphite2.dll"
+    File /oname=libharfbuzz-0.dll "${QT_DEP_BINS}\libharfbuzz-0.dll"
+    File /oname=libiconv-2.dll "${QT_DEP_BINS}\libiconv-2.dll"
+    File /oname=libicudt74.dll "${QT_DEP_BINS}\libicudt74.dll"
+    File /oname=libicuin74.dll "${QT_DEP_BINS}\libicuin74.dll"
+    File /oname=libicuio74.dll "${QT_DEP_BINS}\libicuio74.dll"
+    File /oname=libicutest74.dll "${QT_DEP_BINS}\libicutest74.dll"
+    File /oname=libicutu74.dll "${QT_DEP_BINS}\libicutu74.dll"
+    File /oname=libicuuc74.dll "${QT_DEP_BINS}\libicuuc74.dll"
+    File /oname=libintl-8.dll "${QT_DEP_BINS}\libintl-8.dll"
+    File /oname=libmd4c.dll "${QT_DEP_BINS}\libmd4c.dll"
+    File /oname=libpcre2-16-0.dll "${QT_DEP_BINS}\libpcre2-16-0.dll"
+    File /oname=libpcre2-8-0.dll "${QT_DEP_BINS}\libpcre2-8-0.dll"
+    File /oname=libpng16-16.dll "${QT_DEP_BINS}\libpng16-16.dll"
+    File /oname=libzstd.dll "${QT_DEP_BINS}\libzstd.dll"
+    File /oname=zlib1.dll "${QT_DEP_BINS}\zlib1.dll"
+!endif
+SectionEnd
+
+Section "un.Install dependies"
+    Delete "$INSTDIR\${DST_BINS}\libgcc_s_dw2-1.dll"
+    Delete "$INSTDIR\${DST_BINS}\libstdc++-6.dll"
+    Delete "$INSTDIR\${DST_BINS}\libwinpthread-1.dll"
+
+!if /FileExists "${QT_DEP_BINS}\libEGL.dll"
+    ; MINGW from Qt
+    Delete "$INSTDIR\${DST_BINS}\libEGL.dll"
+    Delete "$INSTDIR\${DST_BINS}\libGLESv2.dll"
+!else
+    ; MSYS2
+    Delete "$INSTDIR\${DST_BINS}\libbrotlicommon.dll"
+    Delete "$INSTDIR\${DST_BINS}\libbrotlidec.dll"
+    Delete "$INSTDIR\${DST_BINS}\libbz2-1.dll"
+    Delete "$INSTDIR\${DST_BINS}\libdouble-conversion.dll"
+    Delete "$INSTDIR\${DST_BINS}\libfreetype-6.dll"
+    Delete "$INSTDIR\${DST_BINS}\libglib-2.0-0.dll"
+    Delete "$INSTDIR\${DST_BINS}\libgraphite2.dll"
+    Delete "$INSTDIR\${DST_BINS}\libharfbuzz-0.dll"
+    Delete "$INSTDIR\${DST_BINS}\libiconv-2.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicudt74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicuin74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicuio74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicutest74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicutu74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libicuuc74.dll"
+    Delete "$INSTDIR\${DST_BINS}\libintl-8.dll"
+    Delete "$INSTDIR\${DST_BINS}\libmd4c.dll"
+    Delete "$INSTDIR\${DST_BINS}\libpcre2-16-0.dll"
+    Delete "$INSTDIR\${DST_BINS}\libpcre2-8-0.dll"
+    Delete "$INSTDIR\${DST_BINS}\libpng16-16.dll"
+    Delete "$INSTDIR\${DST_BINS}\libzstd.dll"
+    Delete "$INSTDIR\${DST_BINS}\zlib1.dll"
+!endif
+
+    RMDir "$INSTDIR\${DST_BINS}"
+    RMDir "$INSTDIR"
+SectionEnd